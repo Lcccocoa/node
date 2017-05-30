99eecb40e966b83d853859f89787e925cbc55509
Lcccocoa <Luoqu520@gmail.com>
2017-05-30 21:54:17 +0800

    passport
    passport

--- controller/userController.js
+++ controller/userController.js
@@ -1,9 +1,35 @@
 //
-var models = require('./models');
+var models = require('../models');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-module.exports = {
-    passport: passport,
+passport.serializeUser(function(user,done){
+    done(null,user.username);
+});
 
+passport.deserializeUser(function(username,done){
+    done(null,{username:username});
+});
+
+//
+passport.use('local', new LocalStrategy(function (username, password, done) {
+    models.User.findOne({
+        where: {
+            username: username
+        }
+    }).then(function (data) {
+        console.log('是否有数据:'+data);
+        if (data.username !== username){
+            return done(null, false, {message: 'incorrect username'});
+        }
+        if (data.password !== password){
+            return done(null, false, {message: 'incorrect password'});
+        }
+        console.log(data);
+        return done(null, data);
+    });
+}));
+
+module.exports = {
+    passport: passport
 };
